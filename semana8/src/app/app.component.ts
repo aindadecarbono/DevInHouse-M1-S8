63d2de23ea7db4b8679d8e8c92a0577622c74a08
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-25 15:40:22 -0300

    ex 1

---
+++ semana8/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'NGF-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <h1>Hello World! :)</h1>
+    
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'semana8';
+}
