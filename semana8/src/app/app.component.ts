ffb5e0e7310439731679a9d645a67563f551aaba
Bruno Coelho <brunocoelhonet@gmail.com>
2022-04-26 15:51:46 -0300

    ex 3

--- semana8/src/app/app.component.ts
+++ semana8/src/app/app.component.ts
@@ -5,16 +5,17 @@ import { Component } from '@angular/core';
   template: `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+  
+    <h1 class="navbar-brand" href="#">NG-FOOD</h1>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">Cardápio</a>
         </li>
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
         <li class="nav-item dropdown">
@@ -30,7 +31,7 @@ import { Component } from '@angular/core';
         </li>
         <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
-        </li>
+        </li> -->
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -41,7 +42,7 @@ import { Component } from '@angular/core';
 </nav>
     
     <h1>Hello World! :)</h1>
-    <button type="button" class="btn btn-primary">Primary</button>
+    
     
   `,
   styles: []
